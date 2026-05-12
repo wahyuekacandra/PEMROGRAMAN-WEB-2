9ddde4753fc47521c972bd3f10e63f57f6693627
wahuekacandra <wahuekacandra@gmail.com>
2026-05-12 20:52:30 +0700

    UPDATE TUGAS

---
+++ LAT-PTM-12/latihan2.php
@@ -0,0 +1,13 @@
+<?php
+$con = mysql_connect("localhost","root","");
+if (!$con)
+{
+    die('Could not connect: ' . mysql_error());
+}
+
+mysql_select_db("lat_dbase", $con);
+
+mysql_query("DELETE FROM tbl_mhs WHERE LastName='Prabowo'");
+
+mysql_close($con);
+?>
